5d20e038455104a61e73e01399b380526963c1dd
rreddykumar310-lang <rreddykumar310@gmail.com>
2026-09-20 19:47:04 +0530

    Update server1.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 const EMAIL_TO = process.env.EMAIL_TO || "rreddykumar310@gmail.com";
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 function createTransporter() {
   if (!process.env.SMTP_HOST || !process.env.SMTP_USER || !process.env.SMTP_PASS) {
